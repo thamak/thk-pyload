3f9f0af2f86dc43c09f8e251709e6d65b131bc3b
Christophe Sophy <christophe.sophy@cgi.com>
2018-11-05 22:38:25 +0100

    Add cleaning dir

--- runFilebot.sh
+++ runFilebot.sh
@@ -5,3 +5,5 @@ docker run --rm -v /media/HDD:/tmp filebot -script fn:amc --output "/tmp" --log 
         --lang fr --action move --conflict override -non-strict "/tmp/DL/complete" \
         --def "movieFormat=/tmp/Movies/{n} {y}/{fn}" --def "seriesFormat=/tmp/TV Shows/{n}/{S+s}/{fn}" \
         --def excludeList=amc.txt
+echo "Clean empty directories !"
+find /opt/pyload/Downloads -mindepth 1 -type d -empty -delete
